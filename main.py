d2a337d0ecc43421a23bd677ae34c4783a966169
Island <islandhuynh@gmail.com>
2021-06-15 06:00:56 -0400

    paddle class added along with player controls

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong by Island Huynh")
+
+player_paddle = Paddle()
+
+screen.onkey(player_paddle.move_up, "Up")
+screen.onkey(player_paddle.move_down, "Down")
+screen.listen()
+
 screen.exitonclick()
