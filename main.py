ff07f63a9ee64cd5e1ec78fcbe7e233e168255f3
Island <islandhuynh@gmail.com>
2021-06-21 01:22:48 -0400

    added pong class with wall and paddle collision

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 from turtle import Screen
 from paddle import Paddle
+from pong import Pong
+from scoreboard import Scoreboard
+import time
+
+PLAYER_START_POSITON = [(-360, 30), (-360, 10), (-360, -10),(-360, -30)]
+COMPUTER_START_POSITION = [(360, 30), (360, 10), (360, -10),(360, -30)]
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong by Island Huynh")
 
-player_paddle = Paddle()
+player_paddle = Paddle(PLAYER_START_POSITON)
+computer_paddle = Paddle(COMPUTER_START_POSITION)
+score_keeper = Scoreboard()
+pong = Pong()
 
+screen.listen()
 screen.onkey(player_paddle.move_up, "Up")
 screen.onkey(player_paddle.move_down, "Down")
-screen.listen()
+
+game_is_on = True
+
+while game_is_on:
+  screen.update()
+  time.sleep(0.1)
+  # while pong.xcor() < 290 and pong.ycor() < 390 and pong.xcor() > -290 and pong.ycor() > -390:
+  pong.move()
+  
+  #Detect collision with wall
+  if pong.ycor() > 280 or pong.ycor() < -280:
+    pong.bounce_y()
+
+  # Detect collision with paddle
+  for seg in player_paddle.paddle:
+    if seg.distance(pong) < 20:
+      pong.bounce_x()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -2,16 +2,15 @@ from turtle import Turtle
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
-PLAYER_START_POSITON = [(-360, 30), (-360, 10), (-360, -10),(-360, -30)]
-COMPUTER_START_POSITION = [(360, 30), (360, 10), (360, -10),(360, -30)]
 
-class Paddle(Turtle):
-  def __init__(self):
+class Paddle:
+  def __init__(self, starting_position):
+    self.starting_positon = starting_position
     self.paddle = []
     self.init_paddle()
 
   def init_paddle(self):
-    for position in PLAYER_START_POSITON:
+    for position in self.starting_positon:
       block = Turtle("square")
       block.color("white")
       block.speed("fastest")
@@ -22,10 +21,10 @@ class Paddle(Turtle):
   def move_up(self):
     if self.paddle[0].ycor() < 290:
       for block_num in range(0, len(self.paddle)):
-        self.paddle[block_num].goto(PLAYER_START_POSITON[block_num][0], self.paddle[block_num].ycor() + MOVE_DISTANCE)
+        self.paddle[block_num].sety(self.paddle[block_num].ycor() + MOVE_DISTANCE)
 
   def move_down(self):
     if self.paddle[len(self.paddle) - 1].ycor() > -290:
       for block_num in range(0, len(self.paddle)):
-        self.paddle[block_num].goto(PLAYER_START_POSITON[block_num][0], self.paddle[block_num].ycor() - MOVE_DISTANCE)
+        self.paddle[block_num].sety( self.paddle[block_num].ycor() - MOVE_DISTANCE)
     
